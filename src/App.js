1ea0734ed56e9ef81fbaae2ac4ec1a80f0596503
Ju Dantas <jdantas@thoughtworks.com>
2020-07-04 02:14:20 -0300

    Added style for title's card

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
 import React from 'react'
 import { Button } from './components/Button/Button'
 import { Card } from './components/Card/Card'
+import { Input } from './components/Input/Input'
+import { InputBox } from './components/InputBox/InputBox'
 
 function App() {
   return (
     <div>
-      <Button label="teste" />
-      <Card>teste</Card>
+      <Card title="Informações da ONG">
+        <Button label="Adicionar outro produto" />
+        <Input label="Nome" placeholder="Ex.: Fralda, fogão, sabão, conta de luz" />
+      </Card>
+      <Card>
+        <InputBox subtitle="Digite aqui o seu nome completo por favor!" label="Nome">
+        </InputBox>
+      </Card>
     </div>
   );
 }
