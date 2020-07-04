dac8816010dd1977175c2f228772571f1683cafc
Ju Dantas <jdantas@thoughtworks.com>
2020-07-04 18:57:40 -0300

    Created button mercado livre component

---
+++ src/components/ButtonMercadoLivre/ButtonMercadoLivre.js
@@ -0,0 +1,6 @@
+import React from 'react'
+import './button-mercado-livre.scss'
+
+export const ButtonMercadoLivre = ({ label }) => {
+  return <button className="mercado-livre-button">{label}</button>
+}
