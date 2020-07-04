f70be0356e76edf2c876d8d7ee8367e5deb74de9
Ju Dantas <jdantas@thoughtworks.com>
2020-07-04 02:21:02 -0300

    Added role for title props in card component

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -4,7 +4,7 @@ import './card.scss'
 export const Card = ({ children, title }) => {
   return (
     <section className="card-box">
-      <h1>{title}</h1>
+      { title ? <h1>{title}</h1> : '' }
       {children}
     </section>
   )
