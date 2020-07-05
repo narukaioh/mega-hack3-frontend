197471e0eb200a2f5f1828012de28c23c08cda7e
Ju Dantas <jdantas@thoughtworks.com>
2020-07-05 00:47:13 -0300

    change routes

--- src/pages/RegisterONGPage/RegisterONGPage.js
+++ src/pages/RegisterONGPage/RegisterONGPage.js
@@ -1,10 +1,21 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import './register-ong-page.scss'
+import { getProducts } from '../../services/getProducts'
 
 export const RegisterONGPage = () => {
+  
+  const [products, setProducts] = useState([])
+
+  useEffect(() => {
+    getProducts("fralda").then(res => {
+      setProducts(res)
+    })
+  }, [])
+
   return (
     <div className="container">
       Ola mundo!
+      { products }
     </div>
   );
 }
--- src/routes/config.js
+++ src/routes/config.js
@@ -1,7 +1,9 @@
+import { Home } from '../pages/Home/Home'
 import { RegisterONGPage } from '../pages/RegisterONGPage/RegisterONGPage'
 import { ComponentsPage } from '../pages/ComponentsPage/ComponentsPage'
 
 export const ROUTES_CONFIG = [
-  { path: '/', exact: true, component: RegisterONGPage },
-  { path: '/components-page', exact: true, component: ComponentsPage }
+  { path: '/', exact: true, component: Home },
+  { path: '/components-page', exact: true, component: ComponentsPage },
+  { path: '/ongs', exact: true, component: RegisterONGPage }
 ];
