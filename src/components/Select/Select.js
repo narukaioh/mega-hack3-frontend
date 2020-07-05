086aee0618b7cce868a84d9f7ee5ef7d3e588f1c
Ju Dantas <jdantas@thoughtworks.com>
2020-07-04 22:19:08 -0300

    Added subtitle in select component

--- src/components/Select/Select.js
+++ src/components/Select/Select.js
@@ -1,8 +1,9 @@
 import React, { useState, useEffect } from 'react'
 import './select.scss'
 import ChevronDown from '../../icons/chevron-down-ico.svg'
+import { InputBox } from '../InputBox/InputBox'
 
-export const Select = ({ options }) => {
+export const Select = ({ options, label, subtitle}) => {
 
   const [selectValue, setSelectValue] = useState({ value: '', label: '' })
   
@@ -18,14 +19,16 @@ export const Select = ({ options }) => {
   },{})
 
   return (
-    <div className="select-container">
-      <select onChange={handlerOnChange}>
-        {options.map(option => <option value={option.value}>{option.label}</option>)}
-      </select>
-      <div className="select-display-value">{ selectValue.label }</div>
-      <span className="select-chevron-bottom bottom">
-        <img src={ChevronDown} alt="chevron down" />
-      </span>
-    </div>
+    <InputBox label={label} subtitle={subtitle}>
+      <div className="select-container">
+        <select onChange={handlerOnChange}>
+          {options.map(option => <option value={option.value}>{option.label}</option>)}
+        </select>
+        <div className="select-display-value">{ selectValue.label }</div>
+        <span className="select-chevron-bottom bottom">
+          <img src={ChevronDown} alt="chevron down" />
+        </span>
+      </div>
+    </InputBox>
   )
 }
