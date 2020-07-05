23ba3b1874068f957631b9089487f00f058ecf85
Ju Dantas <jdantas@thoughtworks.com>
2020-07-05 08:53:06 -0300

    solved some margins differences  and add new icon

--- src/components/IconLabel/IconLabel.js
+++ src/components/IconLabel/IconLabel.js
@@ -1,10 +1,10 @@
 import React from 'react'
 import './icon-label.scss'
 
-export const IconLabel = ({ icon, label }) => {
+export const IconLabel = ({ icon, label, children }) => {
   return (
     <div className="icon-label-content">
       <img src={icon} alt={label} />
-      <span>{label}</span>
+      <span>{children ? children : label}</span>
     </div>)
 }
