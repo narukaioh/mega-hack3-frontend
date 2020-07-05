17845d282813f1d3fe90271ade05a9312c3aca55
Ju Dantas <jdantas@thoughtworks.com>
2020-07-05 09:37:59 -0300

    Removed unused dependencies

--- src/pages/DonatePage/DonatePage.js
+++ src/pages/DonatePage/DonatePage.js
@@ -1,30 +1,7 @@
-import React, { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
-import { Button } from "../../components/Button/Button";
+import React from "react";
 import { Card } from "../../components/Card/Card";
-import { CardIcon } from "../../components/CardIcon/CardIcon";
 import { Input } from "../../components/Input/Input";
-import { InputBox } from "../../components/InputBox/InputBox";
-import { Tag } from "../../components/Tag/Tag";
-import { Tags } from "../../components/Tags/Tags";
-import Shop from "../../icons/shop-icon.svg";
-import Document from "../../icons/document.svg";
-import TagIconOrange from "../../icons/tag-icon-orange.svg";
-import FileUpload from "../../icons/file-upload-icon.svg";
 import { ButtonSecondary } from "../../components/ButtonSecondary/ButtonSecondary";
-import { InputRange } from "../../components/InputRange/InputRange";
-import { InputIcon } from "../../components/InputIcon/InputIcon";
-import { TextArea } from "../../components/TextArea/TextArea";
-import { UploadFile } from "../../components/UploadFile/UploadFile";
-import { ButtonMercadoLivre } from "../../components/ButtonMercadoLivre/ButtonMercadoLivre";
-import { CheckButton } from "../../components/CheckButton/CheckButton";
-import { Select } from "../../components/Select/Select";
-import { ButtonDefault } from "../../components/ButtonDefault/ButtonDefault";
-import { ButtonLight } from "../../components/ButtonLight/ButtonLight";
-import { getONG } from "../../services/getONGs";
-import { NeedsHeader } from "../../components/Needs/NeedsHeader";
-import { Address } from "../../components/Address/Address";
-import { IconLabel } from "../../components/IconLabel/IconLabel";
 import "./donate-page.scss";
 
 export const DonatePage = () => {
