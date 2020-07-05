c066f2b8be9b2a547b3bbfc3a73e0715b211668e
Ju Dantas <jdantas@thoughtworks.com>
2020-07-05 05:46:48 -0300

    added routews for new pages

--- src/routes/config.js
+++ src/routes/config.js
@@ -1,9 +1,12 @@
 import { Home } from '../pages/Home/Home'
 import { RegisterONGPage } from '../pages/RegisterONGPage/RegisterONGPage'
 import { ComponentsPage } from '../pages/ComponentsPage/ComponentsPage'
+import { OngPage } from '../pages/OngPage/OngPage'
 
 export const ROUTES_CONFIG = [
   { path: '/', exact: true, component: Home },
   { path: '/components-page', exact: true, component: ComponentsPage },
-  { path: '/ongs', exact: true, component: RegisterONGPage }
+  { path: '/ongs', exact: true, component: RegisterONGPage },
+  { path: '/ong/:id', component: OngPage },
+  { path: '*', component: Home }
 ];
