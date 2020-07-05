dc5c115d7a4fbd829c118f20ed0382b5595e2af3
Ju Dantas <jdantas@thoughtworks.com>
2020-07-05 18:15:10 -0300

    create map for services

--- src/services/client/client.js
+++ src/services/client/client.js
@@ -5,10 +5,12 @@ const baseURLlocal = 'http://localhost:8000/'
 const baseURL = 'https://megahack3-backend.herokuapp.com/'
 
 const client = axios.create({
-  baseURL: baseURLlocal,
+  baseURL: baseURL,
   headers: {
     Authorization: `bearer ${token}`
   }
 })
+
+export const get = (endpoint) => client(endpoint).then(res => res.data)
 
 export default client
--- src/services/ongs.js
+++ src/services/ongs.js
@@ -1,5 +1,38 @@
-import client from './client/client'
+import client, { get } from "./client/client";
 
-export const getONGs = async () => await client(`/ong`).then(res => res.data)
+export const getONGs = async () =>
+  await get(`/ongs`).then((res) =>
+    res.ongs.map((ong) => {
+      return {
+        id: ong,
+        name: ong.name,
+        image: ong.logo,
+        description: ong.description,
+        tags: ong.tags,
+        locale: ong.city,
+        subject: ong.cause,
+      };
+    })
+  );
 
-export const getONG = async (id) => await client(`ong/${id}`).then(res => res.data)
+export const getONG = async (id) => await get(`ong/${id}`);
+
+export const searchONGs = async (content) =>
+  await get(`ongs?search=${content}`).then((res) => {
+    const result = res.ongs.map((ong) => {
+      if (ong.id) {
+        return {
+          id: ong,
+          name: ong.name,
+          image: ong.logo,
+          description: ong.description,
+          tags: ong.tags,
+          locale: ong.city,
+          subject: ong.cause,
+        };
+      }
+      return null
+    })
+
+    return result[0] === null ? [] : result
+  });
