4705ec851fb8f4aa239f2d443eecbfb7c69fa42c
Ju Dantas <jdantas@thoughtworks.com>
2020-07-05 05:31:37 -0300

    Create variable for testing api mock

--- src/services/client/client.js
+++ src/services/client/client.js
@@ -1,9 +1,11 @@
 import axios from 'axios'
 
 const token = 'megahack3'
+const baseURLlocal = 'http://localhost:8000/'
+const baseURL = 'https://megahack3-backend.herokuapp.com/'
 
 const client = axios.create({
-  baseURL: 'https://megahack3-backend.herokuapp.com/',
+  baseURL: baseURLlocal,
   headers: {
     Authorization: `bearer ${token}`
   }
