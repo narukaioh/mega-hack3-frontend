4d9608bb4f83e38ef53c8f20a84f0192be729152
Ju Dantas <jdantas@thoughtworks.com>
2020-07-08 04:20:54 -0300

    removed all warnings

--- src/components/common/Select/Select.js
+++ src/components/common/Select/Select.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react'
-import ChevronDown from '../../icons/chevron-down-ico.svg'
-import { InputBox } from '../../InputBox/InputBox'
+import ChevronDown from '../../../icons/chevron-down-ico.svg'
+import { InputBox } from '../InputBox/InputBox'
 import './select.scss'
 
 export const Select = ({ options, label, subtitle}) => {
