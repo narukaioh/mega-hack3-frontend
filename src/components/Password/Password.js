e037927cf7132ce794cea5ed2f57b639e0df0a3c
Ju Dantas <jdantas@thoughtworks.com>
2020-07-07 22:18:44 -0300

    Created password component

---
+++ src/components/Password/Password.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import './password.scss'
+import { InputBox } from '../InputBox/InputBox'
+
+export const Password = ({ label, placeholder, subtitle, name, onChange }) => {
+  return (
+    <InputBox label={label} subtitle={subtitle}>
+      <input type="password" onChange={onChange} name={name} placeholder={placeholder} />
+    </InputBox>
+  )
+}
