7ad07f43b3d05a59152a810616ff3f32fd8d3844
Ju Dantas <jdantas@thoughtworks.com>
2020-07-08 02:35:00 -0300

    Removed button unused

--- src/components/ButtonMercadoLivre/ButtonMercadoLivre.js
+++
@@ -1,6 +0,0 @@
-import React from 'react'
-import './button-mercado-livre.scss'
-
-export const ButtonMercadoLivre = ({ label }) => {
-  return <button className="mercado-livre-button">{label}</button>
-}
